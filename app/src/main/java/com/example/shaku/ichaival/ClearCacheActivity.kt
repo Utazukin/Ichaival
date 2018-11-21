afa7bdf55627fadfb05921b9e63de3ecce23de06
Utazukin <45130999+Utazukin@users.noreply.github.com>
2018-11-20 21:42:42 -0600

    Added a toast message when clearing the archive cache.

--- app/src/main/java/com/example/shaku/ichaival/ClearCacheActivity.kt
+++ app/src/main/java/com/example/shaku/ichaival/ClearCacheActivity.kt
@@ -2,12 +2,14 @@ package com.example.shaku.ichaival
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 
 class ClearCacheActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DatabaseReader.clearCache(this)
+        Toast.makeText(applicationContext, "Cleared archive cache", Toast.LENGTH_SHORT).show()
         finish()
     }
 }
