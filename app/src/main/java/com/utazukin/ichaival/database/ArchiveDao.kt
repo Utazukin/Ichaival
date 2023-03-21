9054584feec22eceeb4f82b40d7ed749a61b7519
Utazukin <45130999+Utazukin@users.noreply.github.com>
2023-03-20 22:27:39 -0500

    Fix a crash during database migration if an archive didn't have any tags.

--- app/src/main/java/com/utazukin/ichaival/database/ArchiveDao.kt
+++ app/src/main/java/com/utazukin/ichaival/database/ArchiveDao.kt
@@ -175,7 +175,9 @@ class DatabaseTypeConverters {
             }
         }
 
-        builder.delete(builder.length - 2, builder.length)
+        if (builder.isNotEmpty())
+            builder.delete(builder.length - 2, builder.length)
+
         return builder.toString()
     }
 
